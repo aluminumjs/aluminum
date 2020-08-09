4aed6f7f86bf1f1fe9941ddcb25263eb43618d4a
Ben Zelnick <34197604+ZelnickB@users.noreply.github.com>
2020-08-08 20:38:40 -0400

    [DOCS] Finished updating favicon
    There were several errors in f71854c that prevented some of the intended effects (for instance, the .ico file was empty). This commit corrects these mistakes.

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ html_theme = 'default'
 
 html_logo = './logo-white.png'
 
-html_favicon = './favicon.png'
+html_favicon = './favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
