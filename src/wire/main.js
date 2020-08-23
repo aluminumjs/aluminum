e1bf307fc8ca7256c0763120a60f19a803745d43
Ben Zelnick <34197604+ZelnickB@users.noreply.github.com>
2020-08-23 18:03:17 -0400

    Added auto-redirection to ./index.html
    When a request is made with a URL that ends in /, Wire will automatically serve the index.html file in that directory (if it exists; otherwise, it will respond with a 404 error). An option to disable this feature will be added in the future.
    This commit also adds the `$adjrequrl$` variable for use in custom error pages, which is replaced with the request URL with "index.html" appended if applicable (that is, if the original request has a URL ending in /). The default 404 error page has been updated to use this variable rather than `$requrl$`.

--- src/wire/main.js
+++ src/wire/main.js
@@ -9,7 +9,7 @@ const errorPage = fs.readFileSync('../../defaults/errors/404.html', 'utf8') // D
 const logo = fs.readFileSync('../../logo.svg', 'utf8')
 
 function serverHandler (req, res) {
-  const filename = path.join(__dirname, '..', '..', '..', path.normalize(req.url))
+  let filename = path.join(__dirname, '..', '..', '..', path.normalize(req.url))
   const mimetype = mimeTypes.lookup(filename)
   const reqURLArray = req.url.split('/')
   if (reqURLArray[1] === 'aluminum-internals') {
@@ -33,11 +33,20 @@ function serverHandler (req, res) {
         return res.end()
     }
   }
+  if (reqURLArray[reqURLArray.length - 1] === '') {
+    filename += 'index.html'
+  }
   fs.readFile(filename, function (err, data) {
     if (err) {
+      let adjustedReqURL = req.url
+      if (reqURLArray[reqURLArray.length - 1] === '') {
+        adjustedReqURL += 'index.html'
+      }
       res.writeHead(404, { 'Content-Type': 'html' })
       res.write(
-        errorPage.replace(/\$requrl\$/g, req.url)
+        errorPage
+          .replace(/\$requrl\$/g, req.url)
+          .replace(/\$adjrequrl\$/g, adjustedReqURL)
           .replace(/\$osplatform\$/g, os.platform())
           .replace(/\$ostype\$/g, os.type())
           .replace(/\$osversion\$/g, os.release())
